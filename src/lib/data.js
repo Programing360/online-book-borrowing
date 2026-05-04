381bce63ff5ca68b9e0c76aa790a937f9bcf274b
Md Limon <fhlimon360@gmail.com>
2026-05-04 07:21:21 +0600

    added public api

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,11 +1,11 @@
 export const allBooksData = async () => {
-  const data = await fetch("https://online-book-server-n8my.onrender.com/Data");
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/Data`);
   const res = await data.json();
   return res;
 };
 
 export const allBooksId = async (id) => {
-  const bookDetails = await fetch(`https://online-book-server-n8my.onrender.com/Data/${id}`);
+  const bookDetails = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/Data/${id}`);
   const res = await bookDetails.json();
   return res
 };
